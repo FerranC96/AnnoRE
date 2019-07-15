49da425a3fbeaa6954c71ff9f5b536256e695af4
FerranC96 <ferricaro@hotmail.com>
2019-07-15 13:26:13 +0100

    First batch of scripts

---
+++ scripts/functann/read_input.py
@@ -0,0 +1,21 @@
+#First uit should read a simple .vcf such as ../../../example_data/homo_sapiens_GRCh38.vcf
+#This comes sfrom VEP exemple tutorial
+import csv
+
+vcf_file = "../../../example_data/homo_sapiens_GRCh38.vcf"
+
+def get_ids(input_file, format = "vcf"):
+    SNP_ids = []
+    if format == "vcf": #Getting rs ID from the third column (TAB SEPARATED in vcf)
+        with open(input_file, newline="") as input:
+            #if format = "vcf":
+            input = csv.reader(input, delimiter="\t")
+            for i in input:
+                if not i[0].startswith("#"):
+                    SNP_ids.append(i[2])
+    else:
+        print ("This format hasn't been implemented yet!")
+        return "This format hasn't been implemented yet!"
+    return SNP_ids
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
